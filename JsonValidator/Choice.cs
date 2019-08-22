061db99f6d11674c3e1284178406122372e631bc
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-22 19:40:09 +0300

    Adde Class Choice Allows the use of an array of patterns formed by different Character and Range object to check the first char of a string

---
+++ JsonValidator/Choice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class Choice : IPattern
+    {
+        readonly IPattern[] patterns;
+
+        public Choice(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            foreach (var pattern in patterns)
+            {
+                if (pattern.Match(text))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
