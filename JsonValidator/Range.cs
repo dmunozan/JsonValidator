11d3883189ee2f632b3d41b4e636154d30b766cf
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-22 19:35:01 +0300

    Added class Range Allows to check if the first char of a string matches the object range defined by start - end

---
+++ JsonValidator/Range.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class Range : IPattern
+    {
+        readonly char start;
+        readonly char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return this.start <= text[0] && text[0] <= this.end;
+        }
+    }
+}
