2207339fdaa6a4b698339dd6c4358c2f35d640d3
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-22 19:32:21 +0300

    Initial setup for the project

---
+++ JsonValidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Entry point");
+        }
+    }
+}
