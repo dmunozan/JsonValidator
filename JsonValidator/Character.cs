97f1dd4ad5c0607b91df7646566959abc92ac9ab
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-22 19:31:26 +0300

    Added class Character Allows to check if the first char of a string matches the object pattern

---
+++ JsonValidator/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class Character : IPattern
+    {
+        private readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return text[0] == this.pattern;
+        }
+    }
+}
