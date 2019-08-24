9276cee72681c96093af0d78127a7f30005689ec
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-24 19:23:53 +0300

    Modified Match Changed returning type to IMatch from bool Changed the return statements to comply with the new interface

--- JsonValidator/Character.cs
+++ JsonValidator/Character.cs
@@ -11,14 +11,14 @@ namespace JsonValidator
             this.pattern = pattern;
         }
 
-        public bool Match(string text)
+        public IMatch Match(string text)
         {
             if (string.IsNullOrEmpty(text))
             {
-                return false;
+                return new Match(false, text);
             }
 
-            return text[0] == this.pattern;
+            return (text[0] == this.pattern) ? new Match(true, text.Substring(1)) : new Match(false, text);
         }
     }
 }
