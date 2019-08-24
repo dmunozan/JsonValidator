b8bd446707c0269811d852b8c1e08b1637e5a234
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-24 20:39:27 +0300

    Added Sequence class Modified Match method to satisfy first test

---
+++ JsonValidator/Sequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class Sequence : IPattern
+    {
+        readonly IPattern[] patterns;
+
+        public Sequence(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            return new Match(true, text.Substring(patterns.Length));
+        }
+    }
+}
