02905e6146cc22661a10ac3e4f272c12495fc1af
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-24 19:17:48 +0300

    Added Class Match Implements the Interface IMatch

---
+++ JsonValidator/Match.cs
@@ -0,0 +1,24 @@
+﻿namespace JsonValidator
+{
+    public class Match : IMatch
+    {
+        readonly bool success;
+        readonly string remainingText;
+
+        public Match(bool success, string remainingText)
+        {
+            this.success = success;
+            this.remainingText = remainingText;
+        }
+
+        public bool Success()
+        {
+            return this.success;
+        }
+
+        public string RemainingText()
+        {
+            return this.remainingText;
+        }
+    }
+}
