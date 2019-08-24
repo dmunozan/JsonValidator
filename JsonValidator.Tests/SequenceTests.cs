cfbbbc1a5b03b6e84824ea7e7fe322fb8ff47b49
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-24 20:40:09 +0300

    Added SequenceTests class Added MatchWhenStartsWithABShouldReturnTrueAndRemainingText

---
+++ JsonValidator.Tests/SequenceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace JsonValidator.Tests
+{
+    public class SequenceTests
+    {
+        static Sequence ab = new Sequence(
+            new Character('a'),
+            new Character('b')
+        );
+
+        [Fact]
+        public void MatchWhenStartsWithABShouldReturnTrueAndRemainingText()
+        {
+            Match expectedResult = new Match(true, "cd");
+            Match obtainedResult = (Match)ab.Match("abcd");
+            string expectedResultString = expectedResult.Success() + ", " + expectedResult.RemainingText();
+            string obtainedResultString = obtainedResult.Success() + ", " + obtainedResult.RemainingText();
+
+            Assert.Equal(expectedResultString, obtainedResultString);
+        }
+    }
+}
