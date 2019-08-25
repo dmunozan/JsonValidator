434e9759bd083a36305eeadd9106971373fa663c
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-25 12:20:18 +0300

    Refactored tests Changed MatchWhenNullOnHexShouldReturnFalseAndText to double assert and obtainedResult type to IMatch to avoid casting

--- JsonValidator.Tests/ChoiceTests.cs
+++ JsonValidator.Tests/ChoiceTests.cs
@@ -165,12 +165,10 @@ namespace JsonValidator.Tests
         [Fact]
         public void MatchWhenNullOnHexShouldReturnFalseAndText()
         {
-            Match expectedResult = new Match(false, null);
-            Match obtainedResult = (Match)hex.Match(null);
-            string expectedResultString = expectedResult.Success() + ", " + expectedResult.RemainingText();
-            string obtainedResultString = obtainedResult.Success() + ", " + obtainedResult.RemainingText();
+            IMatch obtainedResult = hex.Match(null);
 
-            Assert.Equal(expectedResultString, obtainedResultString);
+            Assert.False(obtainedResult.Success());
+            Assert.Null(obtainedResult.RemainingText());
         }
     }
 }
