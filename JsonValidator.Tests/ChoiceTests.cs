0cc371d76a1d3a12091ce99ed6388fa23ebc6020
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-25 12:12:23 +0300

    Refactored tests Changed MatchWhenStartsWithFirstCharOfHexDigitRangeShouldReturnTrueAndRemainingText to double assert and obtainedResult type to IMatch to avoid casting

--- JsonValidator.Tests/ChoiceTests.cs
+++ JsonValidator.Tests/ChoiceTests.cs
@@ -84,12 +84,10 @@ namespace JsonValidator.Tests
         [Fact]
         public void MatchWhenStartsWithFirstCharOfHexDigitRangeShouldReturnTrueAndRemainingText()
         {
-            Match expectedResult = new Match(true, "2");
-            Match obtainedResult = (Match)hex.Match("12");
-            string expectedResultString = expectedResult.Success() + ", " + expectedResult.RemainingText();
-            string obtainedResultString = obtainedResult.Success() + ", " + obtainedResult.RemainingText();
+            IMatch obtainedResult = hex.Match("12");
 
-            Assert.Equal(expectedResultString, obtainedResultString);
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("2", obtainedResult.RemainingText());
         }
 
         [Fact]
