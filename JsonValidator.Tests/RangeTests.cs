43042b591380f38d716bccfe116bb40fecebb93e
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-25 11:55:16 +0300

    Refactored tests Changed MatchWhenLastCharOfRangeShouldReturnTrueAndRemainingText to double assert and obtainedResult type to IMatch to avoid casting

--- JsonValidator.Tests/RangeTests.cs
+++ JsonValidator.Tests/RangeTests.cs
@@ -35,12 +35,10 @@ namespace JsonValidator.Tests
         [Fact]
         public void MatchWhenLastCharOfRangeShouldReturnTrueAndRemainingText()
         {
-            Match expectedResult = new Match(true, "ab");
-            Match obtainedResult = (Match)new Range('a', 'f').Match("fab");
-            string expectedResultString = expectedResult.Success() + ", " + expectedResult.RemainingText();
-            string obtainedResultString = obtainedResult.Success() + ", " + obtainedResult.RemainingText();
-
-            Assert.Equal(expectedResultString, obtainedResultString);
+            IMatch obtainedResult = new Range('a', 'f').Match("fab");
+            
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("ab", obtainedResult.RemainingText());
         }
 
         [Fact]
