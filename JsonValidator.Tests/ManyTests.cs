9871ed00131b318cbd8e5f7103d47d21a25c2b39
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-26 21:09:32 +0300

    Created class and added inital test MatchWhenOneOcurrenceShouldReturnTrueAndRemaningText

---
+++ JsonValidator.Tests/ManyTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace JsonValidator.Tests
+{
+    public class ManyTests
+    {
+        [Fact]
+        public void MatchWhenOneOcurrenceShouldReturnTrueAndRemaningText()
+        {
+            IMatch obtainedResult = new Many(new Character('a')).Match("abc");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("bc", obtainedResult.RemainingText());
+        }
+    }
+}
