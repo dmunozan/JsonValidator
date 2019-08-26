85e35338ad1b4b56ee34db206de713fe7365a2a4
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-26 11:58:06 +0300

    Added test Added MatchWhenStartsWithFirstCharacterShouldReturnTrueAndRemainingText

--- JsonValidator.Tests/AnyTests.cs
+++ JsonValidator.Tests/AnyTests.cs
@@ -5,5 +5,13 @@ namespace JsonValidator.Tests
 {
     public class AnyTests
     {
+        [Fact]
+        public void MatchWhenStartsWithFirstCharacterShouldReturnTrueAndRemainingText()
+        {
+            IMatch obtainedResult = new Any("eE").Match("ea");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("a", obtainedResult.RemainingText());
+        }
     }
 }
