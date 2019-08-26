0e0b130ad50964a475be77e65d5d8c27ae031534
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-26 13:35:23 +0300

    Created class and added Match method used if to check for equality

---
+++ JsonValidator/Text.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class PrefixText
+    {
+        readonly string prefix;
+
+        public PrefixText(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (prefix == text)
+            {
+                return new Match(true, "");
+            }
+
+            return new Match(false, text);
+        }
+    }
+}
