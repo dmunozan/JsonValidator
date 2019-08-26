128979dba8419635120055db31d92cb754237e73
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-26 21:34:16 +0300

    Corrected test name and added test Corrected to MatchWhenStartsWithOneOccurrenceShouldReturnTrueAndRemainingText Added MatchWhenStartsWithMoreThanOneOccurrenceShouldReturnTrueAndRemainingText

--- JsonValidator.Tests/ManyTests.cs
+++ JsonValidator.Tests/ManyTests.cs
@@ -6,9 +6,18 @@ namespace JsonValidator.Tests
     public class ManyTests
     {
         [Fact]
-        public void MatchWhenStartsWithOneOcurrenceShouldReturnTrueAndRemaningText()
+        public void MatchWhenStartsWithOneOccurrenceShouldReturnTrueAndRemainingText()
         {
             IMatch obtainedResult = new Many(new Character('a')).Match("abc");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("bc", obtainedResult.RemainingText());
+        }
+
+        [Fact]
+        public void MatchWhenStartsWithMoreThanOneOccurrenceShouldReturnTrueAndRemainingText()
+        {
+            IMatch obtainedResult = new Many(new Character('a')).Match("aaaabc");
 
             Assert.True(obtainedResult.Success());
             Assert.Equal("bc", obtainedResult.RemainingText());
