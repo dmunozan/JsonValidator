3f2fca73b7493bd4d30fddd1dd9cf173e76811a1
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-26 20:54:17 +0300

    Added (Already passing) tests MatchWhenNoStartsWithStringShouldReturnFalseAndText MatchWhenEmptyStringShouldReturnFalseAndText MatchWhenNullShouldReturnFalseAndText

--- JsonValidator.Tests/TextTests.cs
+++ JsonValidator.Tests/TextTests.cs
@@ -22,5 +22,32 @@ namespace JsonValidator.Tests
             Assert.True(obtainedResult.Success());
             Assert.Equal("X", obtainedResult.RemainingText());
         }
+
+        [Fact]
+        public void MatchWhenNoStartsWithStringShouldReturnFalseAndText()
+        {
+            IMatch obtainedResult = new Text("true").Match("false");
+
+            Assert.False(obtainedResult.Success());
+            Assert.Equal("false", obtainedResult.RemainingText());
+        }
+
+        [Fact]
+        public void MatchWhenEmptyStringShouldReturnFalseAndText()
+        {
+            IMatch obtainedResult = new Text("true").Match("");
+
+            Assert.False(obtainedResult.Success());
+            Assert.Equal("", obtainedResult.RemainingText());
+        }
+
+        [Fact]
+        public void MatchWhenNullShouldReturnFalseAndText()
+        {
+            IMatch obtainedResult = new Text("true").Match(null);
+
+            Assert.False(obtainedResult.Success());
+            Assert.Null(obtainedResult.RemainingText());
+        }
     }
 }
