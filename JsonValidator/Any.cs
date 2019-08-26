4c54cda6ffbdb63899e5d891f8a4fbc6cfe1e27c
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-26 11:58:51 +0300

    Added constructor and Match method

--- JsonValidator/Any.cs
+++ JsonValidator/Any.cs
@@ -4,5 +4,17 @@ namespace JsonValidator
 {
     public class Any
     {
+        readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            Console.WriteLine(accepted + text);
+            return new Match(true, "a");
+        }
     }
 }
