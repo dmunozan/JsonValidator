ee3b58311d4274d9b353ac3bb43e8a7cb5841b20
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-26 21:10:43 +0300

    Created class and added basic code Added constructor Added method Match checking for pattern

---
+++ JsonValidator/Many.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class Many
+    {
+        readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
