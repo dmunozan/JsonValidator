0b8f166a3edc87e12a4ba4d77fcb1169626e0461
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-27 20:47:56 +0300

    Added (already passing) tests MatchWhenStringFitThePatternPlusSeparatorShouldReturnTrueAndSeparator MatchWhenStartFitElementPlusOtherCharactersShouldReturnTrueAndRemainingText

--- JsonValidator.Tests/ListTests.cs
+++ JsonValidator.Tests/ListTests.cs
@@ -13,5 +13,23 @@ namespace JsonValidator.Tests
             Assert.True(obtainedResult.Success());
             Assert.Equal("", obtainedResult.RemainingText());
         }
+
+        [Fact]
+        public void MatchWhenStringFitThePatternPlusSeparatorShouldReturnTrueAndSeparator()
+        {
+            IMatch obtainedResult = new List(new Range('0', '9'), new Character(',')).Match("1,2,3,");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal(",", obtainedResult.RemainingText());
+        }
+
+        [Fact]
+        public void MatchWhenStartFitElementPlusOtherCharactersShouldReturnTrueAndRemainingText()
+        {
+            IMatch obtainedResult = new List(new Range('0', '9'), new Character(',')).Match("1a");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("a", obtainedResult.RemainingText());
+        }
     }
 }
