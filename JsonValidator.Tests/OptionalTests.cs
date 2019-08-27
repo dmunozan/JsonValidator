aa66329f4e7e5db512f2a2344bbd7c6ec6d33168
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-27 12:16:32 +0300

    Created class and added test MatchWhenStartsWithOptionalPatternShouldReturnTrueAndRemainingText

---
+++ JsonValidator.Tests/OptionalTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace JsonValidator.Tests
+{
+    public class OptionalTests
+    {
+        [Fact]
+        public void MatchWhenStartsWithOptionalPatternShouldReturnTrueAndRemainingText()
+        {
+            IMatch obtainedResult = new Optional(new Character('a')).Match("abc");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("bc", obtainedResult.RemainingText());
+        }
+    }
+}
