6b22835a9bb1270e60e15adadbf3157bb9ec2f71
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-27 12:17:26 +0300

    Created class. Added constructor and Match method

---
+++ JsonValidator/Optional.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class Optional
+    {
+        readonly IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
