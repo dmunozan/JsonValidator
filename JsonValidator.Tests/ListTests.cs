0ef807bd8923bad026bcaadb7bf322b1bc619008
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-27 14:57:22 +0300

    Created class. Added initial test

---
+++ JsonValidator.Tests/ListTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace JsonValidator.Tests
+{
+    public class ListTests
+    {
+        [Fact]
+        public void MatchWhenAllStringFitThePatternShouldReturnTrueAndEmptyString()
+        {
+            IMatch obtainedResult = new List(new Range('0', '9'), new Character(',')).Match("1,2,3");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("", obtainedResult.RemainingText());
+        }
+    }
+}
