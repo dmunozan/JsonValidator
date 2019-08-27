20a29b4367087b350309858dbd0071a5e9fd279e
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-27 13:22:52 +0300

    Created class and added initial test MatchWhenAllCharactersFitThePatternShouldReturnTrueAndEmptyString

---
+++ JsonValidator.Tests/OneOrMoreTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace JsonValidator.Tests
+{
+    public class OneOrMoreTests
+    {
+        [Fact]
+        public void MatchWhenAllCharactersFitThePatternShouldReturnTrueAndEmptyString()
+        {
+            IMatch obtainedResult = new OneOrMore(new Range('0', '9')).Match("123");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("", obtainedResult.RemainingText());
+        }
+    }
+}
