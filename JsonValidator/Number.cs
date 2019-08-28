8c6ecc4609a782ed56e568c2f6fcc9e5db8ca799
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-28 13:13:16 +0300

    Created class. Added constructor and Match method

---
+++ JsonValidator/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class Number
+    {
+        readonly IPattern pattern;
+
+        public Number()
+        {
+            this.pattern = new Range('0', '9');
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
