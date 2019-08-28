e1934a49741cb265b98e57ff70b401584b283f46
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-28 13:12:53 +0300

    Created class and added initial test MatchWhenOneNumberShouldReturnTrueAndEmptyString

---
+++ JsonValidator.Tests/NumberTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace JsonValidator.Tests
+{
+    public class NumberTests
+    {
+        [Fact]
+        public void MatchWhenOneNumberShouldReturnTrueAndEmptyString()
+        {
+            IMatch obtainedResult = new Number().Match("2");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("", obtainedResult.RemainingText());
+        }
+    }
+}
