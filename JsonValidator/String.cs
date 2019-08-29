83fa26920fef296ee072d49e13fda87a31a7b27e
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-29 11:43:26 +0300

    Created class. Added constructor and Match method.

---
+++ JsonValidator/String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JsonValidator
+{
+    public class String : IPattern
+    {
+        readonly IPattern pattern;
+
+        public String()
+        {
+            this.pattern = new Sequence(new Character('"'), new Character('"'));
+        }
+
+        public IMatch Match(string text)
+        {
+            return this.pattern.Match(text);
+        }
+    }
+}
