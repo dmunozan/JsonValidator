50648cf5cacbefb957c30d22693e4eaed58869c9
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-29 10:59:58 +0300

    Modified constructor Created sign and exponent Added optional(exponent) to the pattern assignment

--- JsonValidator/Number.cs
+++ JsonValidator/Number.cs
@@ -13,8 +13,10 @@ namespace JsonValidator
             OneOrMore digits = new OneOrMore(digit);
             Sequence integer = new Sequence(new Optional(new Character('-')), new Choice(new Sequence(oneNine, digits), digit));
             Sequence fraction = new Sequence(new Character('.'), digits);
+            Any sign = new Any("-+");
+            Sequence exponent = new Sequence(new Any("eE"), new Optional(sign), digits);
 
-            this.pattern = new Sequence(integer, new Optional(fraction));
+            this.pattern = new Sequence(integer, new Optional(fraction), new Optional(exponent));
         }
 
         public IMatch Match(string text)
