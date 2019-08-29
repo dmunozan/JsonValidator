78f0b4f570e5848cb2b80138b624cfe4a078395d
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-29 11:42:50 +0300

    Added initial test MatchWhenTwoDoubleQuotesShouldReturnTrueAndEmptyString

---
+++ JsonValidator.Tests/StringTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace JsonValidator.Tests
+{
+    public class StringTests
+    {
+        [Fact]
+        public void MatchWhenTwoDoubleQuotesShouldReturnTrueAndEmptyString()
+        {
+            IMatch obtainedResult = new String().Match("\"\"");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("", obtainedResult.RemainingText());
+        }
+    }
+}
