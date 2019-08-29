75ab53599663d8697e7725882654975a5ddc3e24
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-29 13:16:58 +0300

    Modified constructor Added characters to pattern assignment which is composed by the allowed char ranges

--- JsonValidator/String.cs
+++ JsonValidator/String.cs
@@ -8,7 +8,14 @@ namespace JsonValidator
 
         public String()
         {
-            this.pattern = new Sequence(new Character('"'), new Character('"'));
+            const char LastCharacter = (char)ushort.MaxValue;
+            Any spaceExclamation = new Any(" !");
+            Range hashtagToLeftSquareBracket = new Range('#', '[');
+            Range rightSquareBracketToLastCharacter = new Range(']', LastCharacter);
+            Choice character = new Choice(spaceExclamation, hashtagToLeftSquareBracket, rightSquareBracketToLastCharacter);
+            Many characters = new Many(character);
+
+            this.pattern = new Sequence(new Character('"'), characters, new Character('"'));
         }
 
         public IMatch Match(string text)
