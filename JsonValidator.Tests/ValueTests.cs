82fa597fc4190216ca26d7d5f0efd63763301cc2
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-31 19:00:07 +0300

    Created class and added initial test MatchWhenNullTextShouldReturnTrueAndRemainingText

---
+++ JsonValidator.Tests/ValueTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace JsonValidator.Tests
+{
+    public class ValueTests
+    {
+        [Fact]
+        public void MatchWhenNullTextShouldReturnTrueAndRemainingText()
+        {
+            IMatch obtainedResult = new Value().Match("null");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("", obtainedResult.RemainingText());
+        }
+    }
+}
