acbed2097072efc72d6c72d2de129d6727ed39da
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-31 23:45:02 +0300

    Modified Main Added code to check the text contained in a text file and print Valid or Invalid if it's a formatted json text

--- JsonValidator/Program.cs
+++ JsonValidator/Program.cs
@@ -4,9 +4,20 @@ namespace JsonValidator
 {
     public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Entry point");
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine(@"Error: Provide the path to the file as argument. Ex. Program C:\Users\Public\TestFolder\WriteText.txt");
+                return;
+            }
+
+            string text = System.IO.File.ReadAllText(args[0]);
+
+            IMatch jsonMatch = new Value().Match(text);
+
+            Console.WriteLine(jsonMatch.Success() ? "Valid" : "Invalid");
+            Console.Read();
         }
     }
 }
