ebe9bc582f979f7e3e10195e7d1fdc7b2119a488
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-08-31 19:14:32 +0300

    Added test MatchWhenNumberShouldReturnTrueAndRemainingText

--- JsonValidator.Tests/ValueTests.cs
+++ JsonValidator.Tests/ValueTests.cs
@@ -31,5 +31,14 @@ namespace JsonValidator.Tests
             Assert.True(obtainedResult.Success());
             Assert.Equal("", obtainedResult.RemainingText());
         }
+
+        [Fact]
+        public void MatchWhenNumberShouldReturnTrueAndRemainingText()
+        {
+            IMatch obtainedResult = new Value().Match("-2.58e+058");
+
+            Assert.True(obtainedResult.Success());
+            Assert.Equal("", obtainedResult.RemainingText());
+        }
     }
 }
